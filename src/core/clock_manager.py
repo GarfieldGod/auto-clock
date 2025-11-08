35868629c268aef001f01c416a28fd78ff210223
GarfieldGod <1351763110@qq.com>
2025-11-08 16:44:22 +0800

    Add const and fix multiple task delete
    Signed-off-by: GarfieldGod <1351763110@qq.com>

--- src/core/clock_manager.py
+++ src/core/clock_manager.py
@@ -8,6 +8,7 @@ from platformdirs import user_data_dir
 from src.utils.log import Log
 from test.test import run_test
 from src.core.auto_clock import AutoClock, Config
+from src.utils.const import Key
 
 DataRoot = user_data_dir("data", "auto-clock")
 
@@ -24,26 +25,26 @@ class ClockManager:
                 ok = ClockManager.check_data(data)
                 if not ok:
                     raise Exception("Unknow Error")
-                self.user_name = data["user_name"]
-                self.user_password = data["user_password"]
-                self.driver_path = data["driver_path"]
+                self.user_name = data[Key.UserName]
+                self.user_password = data[Key.UserPassword]
+                self.driver_path = data[Key.DriverPath]
                 if self.driver_path is None:
                     self.driver_path = get_driver_path()
                 if self.driver_path is None:
                     Log.error("Driver Path Error")
                     raise Exception("No driver path")
 
-                self.always_retry_check_box = data["always_retry_check_box"]
+                self.always_retry_check_box = data[Key.AlwaysRetry]
                 retry_times = None
                 tolerance_angle = None
                 try:
-                    retry_times = int(data.get("captcha_retry_times"))
-                    tolerance_angle = int(data.get("captcha_tolerance_angle"))
+                    retry_times = int(data.get(Key.CaptchaRetryTimes))
+                    tolerance_angle = int(data.get(Key.CaptchaToleranceAngle))
                 except Exception as e:
                     Log.error(e)
                 self.captcha_retry_times = None if retry_times is None else retry_times
                 self.captcha_tolerance_angle = None if tolerance_angle is None else tolerance_angle
-                self.captcha_failed_email = data.get("captcha_failed_email")
+                self.captcha_failed_email = data.get(Key.NotificationEmail)
 
                 self.status = True
         except Exception as e:
@@ -87,20 +88,20 @@ class ClockManager:
 
     @staticmethod
     def check_data(data):
-        Log.info(f"user_name: [{data["user_name"]}]")
-        if data["user_name"] == "":
+        Log.info(f"user_name: [{data[Key.UserName]}]")
+        if data[Key.UserName] == Key.Empty:
             Log.error("[username] is empty.")
             raise Exception("[username] is empty.")
 
-        if data["user_password"] == "" :
+        if data[Key.UserPassword] == Key.Empty :
             Log.error("[password] is empty.")
             raise Exception("[password] is empty.")
 
-        if data["driver_path"] == "":
+        if data[Key.DriverPath] == Key.Empty:
             Log.error("[driver path] is empty.")
             raise Exception("[driver path] is empty.")
 
-        if not os.path.exists(data["driver_path"]):
+        if not os.path.exists(data[Key.DriverPath]):
             Log.error("[driver path] is invalid.")
             raise Exception("[driver path] is invalid.")
 
